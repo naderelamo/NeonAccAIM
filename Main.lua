6d7a6c9358f1c830264dcd905cde76e39cd47911
naderelamo <118464584+naderelamo@users.noreply.github.com>
2026-03-16 22:23:08 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -905,28 +905,3 @@ FunctionsSection:AddButton({
 		setclipboard("https://github.com/Exunys/AirHub")
 	end
 })
-
---// AirHub V2 Prompt
-
-do
-	local Aux = Instance.new("BindableFunction")
-    
-	Aux.OnInvoke = function(Answer)
-		if Answer == "No" then
-			return
-		end
-
-		Library.Unload()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Main.lua"))()
-	end
-
-	game.StarterGui:SetCore("SendNotification", {
-		Title = "🎆  AirHub V2  🎆",
-		Text = "Would you like to use the new AirHub V2 script?",
-		Button1 = "Yes",
-		Button2 = "No",
-		Duration = 1 / 0,
-		Icon = "rbxassetid://6238537240",
-		Callback = Aux
-	})
-end
